3946acb87fdffe64dc5ee2b353a3abdf3d5435bd
momowzen <arianthonyungsod@gmail.com>
2026-07-27 23:57:28 +0900

    Update playing status to show help command

--- index.js
+++ index.js
@@ -956,7 +956,7 @@ client.once('clientReady', async () => {
     console.error('Slash command registration error:', e);
   }
 
-  client.user.setActivity('/kill /bl /ut', { type: 0 });
+  client.user.setActivity('astra / 도움 / へるぷ — Show help', { type: 0 });
 
 });
 
