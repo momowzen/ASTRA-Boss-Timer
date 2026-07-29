d86beabb898208f0b13275f50845ed5cbbc22f27
momowzen <arianthonyungsod@gmail.com>
2026-07-29 16:47:41 +0900

    Change English TTS voice to AriaNeural

--- voice.js
+++ voice.js
@@ -14,7 +14,7 @@ let TTS_SPAWN_IN, TTS_SPAWNED, TTS_DEFEATED;
 let WORLD_BOSS_TIMES, TTS_WORLD_BOSS_IN, TTS_WORLD_BOSS_SPAWNED;
 
 const EDGE_VOICES = {
-  en: 'en-US-AvaNeural',
+  en: 'en-US-AriaNeural',
   ko: 'ko-KR-SunHiNeural',
   ja: 'ja-JP-NanamiNeural',
 };
